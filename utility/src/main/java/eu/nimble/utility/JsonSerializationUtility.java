f8fe48afbc32b939df35cf23bd852e544f06be29
Suat Gonul <suat@srdc.com.tr>
2018-04-24 19:17:44 +0300

    added rest-client submodule; created json serializaion utility

--- utility/src/main/java/eu/nimble/utility/JsonSerializationUtility.java
+++ utility/src/main/java/eu/nimble/utility/JsonSerializationUtility.java
@@ -1,7 +1,27 @@
 package eu.nimble.utility;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by suat on 24-Apr-18.
  */
 public class JsonSerializationUtility {
+    private static Logger logger = LoggerFactory.getLogger(JsonSerializationUtility.class);
+
+    public static <T> T deserializeContent(InputStream serializedContent, TypeReference<T> typeReference) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            T deserializedObject = mapper.readValue(serializedContent, typeReference);
+            return deserializedObject;
+        } catch (IOException e) {
+            logger.error("Failed to deserialize the stream: ", e);
+            throw e;
+        }
+    }
 }
