bd13309acc7e5b480a1feddde386a2797744ab2f
Suat Gonul <suat@srdc.com.tr>
2018-07-26 18:07:14 +0300

    added method to identity client for getting all parties

--- rest-client/src/main/java/eu/nimble/common/rest/identity/IdentityClient.java
+++ rest-client/src/main/java/eu/nimble/common/rest/identity/IdentityClient.java
@@ -2,10 +2,10 @@ package eu.nimble.common.rest.identity;
 
 import feign.Response;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Johannes Innerbichler on 02/05/17.
@@ -18,4 +18,7 @@ public interface IdentityClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/parties/{partyIds}", produces = "application/json")
     Response getParties(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyIds") String partyIds);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/party/all", produces = "application/json")
+    Set<String> getAllPartyIds(@RequestHeader("Authorization") String bearerToken, @RequestParam(value = "exclude", required = false) List<String> exclude);
 }
