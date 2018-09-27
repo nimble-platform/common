817dd489f9ccd9d1c8ef2c0177634adaca6b5b88
Suat Gonul <suat@srdc.com.tr>
2018-09-27 10:46:12 +0300

    create enum for quality indicator parameters

---
+++ data-model/ubl-data-model/src/main/java/eu/nimble/service/model/ubl/commonaggregatecomponents/QualityIndicatorParameter.java
@@ -0,0 +1,19 @@
+package eu.nimble.service.model.ubl.commonaggregatecomponents;
+
+/**
+ * Pre-defined quality indicator parameters. However, {@link QualityIndicatorType}s can have other arbitrary parameters.
+ *
+ * Created by suat on 27-Sep-18.
+ */
+public enum QualityIndicatorParameter {
+    NEGOTIATION_TIME,
+    PROFILE_COMPLETENESS,
+    RESPONSE_TIME,
+    TRUST_SCORE,
+    TRADING_VOLUME,
+    SELLER_COMMUNICATION,
+    FULFILLMENT_OF_TERMS,
+    DELIVERY_PACKAGING,
+    NUMBER_OF_TRANSACTIONS,
+    COMPLETENESS_OF_COMPANY_DESCRIPTION
+}
