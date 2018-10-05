8a0b73c51db4f8429d48f4928e68875ab5c87948
Suat Gonul <suat@srdc.com.tr>
2018-10-05 14:20:29 +0300

    feign client for trust moved to common

---
+++ rest-client/src/main/java/eu/nimble/common/rest/trust/TrustClient.java
@@ -0,0 +1,21 @@
+package eu.nimble.common.rest.trust;
+
+import feign.Response;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Feign client for trust microservice.
+ */
+@FeignClient(name = "trust-service", url = "${nimble.trust.url:}")
+public interface TrustClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/version", produces = "application/json")
+    Response getVersion();
+
+    @RequestMapping(method = RequestMethod.GET, value = "party/{partyId}/trust", produces = MediaType.APPLICATION_JSON_VALUE)
+    Response obtainPartyTrustValues(@PathVariable("partyId") String partyId, @RequestHeader("Authorization") String bearerToken);
+}
