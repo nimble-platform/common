2cdfdbad021f62c3f83330b3ae08ddb9742787ac
Suat Gonul <suat@srdc.com.tr>
2018-12-25 14:12:54 +0300

    moved httpresponseutil to common

---
+++ utility/src/main/java/eu/nimble/utility/HttpResponseUtil.java
@@ -0,0 +1,52 @@
+package eu.nimble.utility;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Utility methods to log erroneous cases in REST services
+ *
+ * Created by suat on 16-May-18.
+ */
+public class HttpResponseUtil {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponseUtil.class);
+
+    public static ResponseEntity createResponseEntityAndLog(String msg, HttpStatus httpStatus) {
+        return createResponseEntityAndLog(msg, null, httpStatus, LogLevel.WARN);
+    }
+
+    public static ResponseEntity createResponseEntityAndLog(String msg, Exception e, HttpStatus httpStatus) {
+        if (httpStatus == HttpStatus.BAD_REQUEST) {
+            return createResponseEntityAndLog(msg, e, httpStatus, LogLevel.WARN);
+        } else {
+            return createResponseEntityAndLog(msg, e, httpStatus, LogLevel.ERROR);
+        }
+    }
+
+
+    public static ResponseEntity createResponseEntityAndLog(String msg, Exception e, HttpStatus httpStatus, LogLevel logLevel) {
+        if(logLevel == null || logLevel == LogLevel.INFO) {
+            if(e != null) {
+                logger.info(msg, e);
+            } else {
+                logger.info(msg);
+            }
+        } else if (logLevel == LogLevel.WARN) {
+            if (e != null) {
+                logger.warn(msg, e);
+            } else {
+                logger.warn(msg);
+            }
+        } else if (logLevel == LogLevel.ERROR) {
+            if (e != null) {
+                logger.error(msg, e);
+            } else {
+                logger.error(msg);
+            }
+        }
+        return ResponseEntity.status(httpStatus).body(msg);
+    }
+}
