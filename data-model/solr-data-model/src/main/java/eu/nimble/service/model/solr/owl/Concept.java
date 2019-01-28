49dc22681722697fa8a886a2522589ed06254558
dglachs <dglachs@gmail.com>
2019-01-28 17:51:40 +0100

    added code field, itemtype extends concept

--- data-model/solr-data-model/src/main/java/eu/nimble/service/model/solr/owl/Concept.java
+++ data-model/solr-data-model/src/main/java/eu/nimble/service/model/solr/owl/Concept.java
@@ -20,6 +20,9 @@ public abstract class Concept implements IConcept {
 	@Id
 	@Indexed(required=true, name=ID_FIELD) 
 	protected String uri;
+	
+	@Indexed(name=CODE_FIELD)
+	protected String code;
 
 	@Indexed(name=LOCAL_NAME_FIELD)
 	protected String localName;
@@ -54,6 +57,7 @@ public abstract class Concept implements IConcept {
 	public static Concept buildFrom(IConcept other) {
 		SimpleConcept c = new Concept.SimpleConcept();
 		c.setUri(other.getUri());
+		c.setCode(other.getCode());
 		c.setLabel(other.getLabel());
 		c.setDescription(other.getDescription());
 		c.setComment(other.getComment());
@@ -208,6 +212,14 @@ public abstract class Concept implements IConcept {
 
 	public void setHiddenLabel(Map<String, Collection<String>> hiddenLabel) {
 		this.hiddenLabel = hiddenLabel;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
 	}
 	
 	
