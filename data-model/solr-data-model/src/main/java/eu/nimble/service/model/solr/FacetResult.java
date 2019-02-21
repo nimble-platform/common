14b461d06ac0839d0841bffd27e65692116a471a
dglachs <dglachs@gmail.com>
2019-02-21 09:06:06 +0100

    added sorting to the facet results

--- data-model/solr-data-model/src/main/java/eu/nimble/service/model/solr/FacetResult.java
+++ data-model/solr-data-model/src/main/java/eu/nimble/service/model/solr/FacetResult.java
@@ -1,7 +1,9 @@
 package eu.nimble.service.model.solr;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class FacetResult extends IndexField {
 
@@ -36,10 +38,23 @@ public class FacetResult extends IndexField {
 	
 	public void addEntry(String label, long count) {
 		if ( entry == null ) {
-			entry = new HashSet<>();
+			entry = new TreeSet<>(NumberComparator.INSTANCE);
 		}
 		entry.add(new Entry(label,count));
 	}
-	
-
+	private static class NumberComparator implements Comparator<Entry> {
+		static NumberComparator INSTANCE = new NumberComparator();
+		@Override
+		public int compare(Entry o1, Entry o2) {
+			if ( o1.getCount() == o2.getCount()) {
+				return o1.getLabel().compareToIgnoreCase(o2.getLabel());
+			}
+			if ( o1.getCount() > o2.getCount()) {
+				return 1;
+			}
+			else {
+				return -1; 
+			}
+		}
+	}
 }
