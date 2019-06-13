9235c97c335f50b32d293ef04894efd29eea5a08
Suat Gonul <suat@srdc.com.tr>
2019-06-13 19:02:09 +0300

    ignored IDocument-based fields in serialization

--- data-model/ubl-data-model/src/main/java/eu/nimble/service/model/ubl/document/IDocument.java
+++ data-model/ubl-data-model/src/main/java/eu/nimble/service/model/ubl/document/IDocument.java
@@ -1,5 +1,6 @@
 package eu.nimble.service.model.ubl.document;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.DocumentReferenceType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.ItemType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyNameType;
@@ -7,17 +8,18 @@ import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyNameType;
 import java.util.List;
 
 public interface IDocument {
+    @JsonIgnore
     String getSellerPartyId();
-
+    @JsonIgnore
     List<PartyNameType> getSellerPartyName();
-
+    @JsonIgnore
     String getBuyerPartyId();
-
+    @JsonIgnore
     List<PartyNameType> getBuyerPartyName();
-
+    @JsonIgnore
     String getDocumentStatus();
-
+    @JsonIgnore
     ItemType getItemType();
-
+    @JsonIgnore
     List<DocumentReferenceType> getAdditionalDocuments();
 }
