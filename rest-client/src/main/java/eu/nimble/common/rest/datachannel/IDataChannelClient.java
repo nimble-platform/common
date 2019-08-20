087542eb516b28f2a6e1ea36f016993496c9f96f
dogukan10 <dogukan10cav@gmail.com>
2019-08-20 16:01:05 +0300

    added createChannel service to IDataChannelClient interface

--- rest-client/src/main/java/eu/nimble/common/rest/datachannel/IDataChannelClient.java
+++ rest-client/src/main/java/eu/nimble/common/rest/datachannel/IDataChannelClient.java
@@ -11,4 +11,7 @@ public interface IDataChannelClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/epc/code/{code}", produces = "application/json")
     Response getEPCCodesForOrder(@RequestHeader("Authorization") String bearerToken, @PathVariable("code") String storeId);
+
+    @RequestMapping(method = RequestMethod.POST, value = "/channel/", produces = "application/json", consumes = "application/json")
+    Response createChannel(@RequestHeader("Authorization") String bearerToken, @RequestBody String request);
 }
