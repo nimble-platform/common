87d68ba2bfab55f42acf4c979ec92a13cf19dd46
Suat Gonul <suat@srdc.com.tr>
2020-11-19 15:58:03 +0300

    Methods to copy content between two same UBL objects

--- utility/src/main/java/eu/nimble/utility/UblUtil.java
+++ utility/src/main/java/eu/nimble/utility/UblUtil.java
@@ -3,6 +3,8 @@ package eu.nimble.utility;
 import eu.nimble.service.model.ubl.catalogue.CatalogueType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.*;
 import eu.nimble.service.model.ubl.commonbasiccomponents.BinaryObjectType;
+import eu.nimble.service.model.ubl.commonbasiccomponents.CodeType;
+import eu.nimble.service.model.ubl.commonbasiccomponents.QuantityType;
 import eu.nimble.service.model.ubl.commonbasiccomponents.TextType;
 
 import java.util.*;
@@ -95,5 +97,188 @@ public class UblUtil {
             binaryObjects.addAll(objects);
         }
         return binaryObjects;
+    }
+
+    public static List<BinaryObjectType> getBinaryObjectsFrom(DemandType demand) {
+        List<BinaryObjectType> binaryObjects = new ArrayList<>();
+        if (demand.getAdditionalDocumentReference() != null) {
+            binaryObjects.add(demand.getAdditionalDocumentReference().getAttachment().getEmbeddedDocumentBinaryObject());
+        }
+        return binaryObjects;
+    }
+
+    /*
+    Methods to copy entity fields from a source to a target entity
+     */
+
+    public static void copy(DemandType source, DemandType target) {
+        if (source.getTitle() == null) {
+            target.setTitle(null);
+        } else {
+            if (target.getTitle() == null) {
+                target.setTitle(new TextType());
+            }
+            copy(source.getTitle(), target.getTitle());
+        }
+
+        if (source.getDescription() == null) {
+            target.setDescription(null);
+        } else {
+            if (target.getDescription() == null) {
+                target.setDescription(new TextType());
+            }
+            copy(source.getDescription(), target.getDescription());
+        }
+
+        target.setDueDateItem(null);
+        target.setDueDate(source.getDueDate());
+
+        if (source.getItemClassificationCode() == null) {
+            target.setItemClassificationCode(null);
+        } else {
+            if (target.getItemClassificationCode() == null) {
+                target.setItemClassificationCode(new CodeType());
+            }
+            copy(source.getItemClassificationCode(), target.getItemClassificationCode());
+        }
+
+        if (source.getCountry() == null) {
+            target.setCountry(null);
+        } else {
+            if (target.getCountry() == null) {
+                target.setCountry(new CountryType());
+            }
+            copy(source.getCountry(), target.getCountry());
+        }
+
+        if (source.getAdditionalDocumentReference() == null) {
+            target.setAdditionalDocumentReference(null);
+        } else {
+            if (target.getAdditionalDocumentReference() == null) {
+                target.setAdditionalDocumentReference(new DocumentReferenceType());
+            }
+            copy(source.getAdditionalDocumentReference(), target.getAdditionalDocumentReference());
+        }
+    }
+
+    public static void copy(TextType source, TextType target) {
+        target.setLanguageID(source.getLanguageID());
+        target.setValue(source.getValue());
+    }
+
+    public static void copy(CodeType source, CodeType target) {
+        target.setListID(source.getListID());
+        target.setName(source.getName());
+        target.setURI(source.getURI());
+        target.setValue(source.getValue());
+    }
+
+    public static void copy(CountryType source, CountryType target) {
+        if (source.getIdentificationCode() == null) {
+            target.setIdentificationCode(null);
+        } else {
+            if (target.getIdentificationCode() == null) {
+                target.setIdentificationCode(new CodeType());
+            }
+            copy(source.getIdentificationCode(), target.getIdentificationCode());
+        }
+
+        if (source.getName() == null) {
+            target.setName(null);
+        } else {
+            if (target.getName() == null) {
+                target.setName(new TextType());
+            }
+            copy(source.getName(), target.getName());
+        }
+    }
+
+    public static void copy(DocumentReferenceType source, DocumentReferenceType target) {
+        target.setID(source.getID());
+        target.setDocumentType(source.getDocumentType());
+        target.setUUID(source.getUUID());
+
+        if (source.getAttachment() == null) {
+            target.setAttachment(new AttachmentType());
+        } else {
+            if (target.getAttachment() == null) {
+                target.setAttachment(new AttachmentType());
+            }
+            copy(source.getAttachment(), target.getAttachment());
+        }
+
+        if (source.getValidityPeriod() == null) {
+            target.setValidityPeriod(null);
+        } else {
+            if (target.getValidityPeriod() == null) {
+                target.setValidityPeriod(new PeriodType());
+            }
+            copy(source.getValidityPeriod(), target.getValidityPeriod());
+        }
+    }
+
+    public static void copy(AttachmentType source, AttachmentType target) {
+        if (source.getEmbeddedDocumentBinaryObject() == null) {
+            target.setEmbeddedDocumentBinaryObject(null);
+        } else {
+            if (target.getEmbeddedDocumentBinaryObject() == null) {
+                target.setEmbeddedDocumentBinaryObject(new BinaryObjectType());
+            }
+            copy(source.getEmbeddedDocumentBinaryObject(), target.getEmbeddedDocumentBinaryObject());
+        }
+
+        if (source.getExternalReference() == null) {
+            target.setExternalReference(null);
+        } else {
+            if (target.getExternalReference() == null) {
+                target.setExternalReference(new ExternalReferenceType());
+            }
+            copy(source.getExternalReference(), target.getExternalReference());
+        }
+    }
+
+    public static void copy(ExternalReferenceType source, ExternalReferenceType target) {
+        target.setURI(source.getURI());
+    }
+
+    public static void copy(BinaryObjectType source, BinaryObjectType target) {
+        target.setFileName(source.getFileName());
+        target.setLanguageID(source.getLanguageID());
+        target.setMimeCode(source.getMimeCode());
+        target.setUri(source.getUri());
+        target.setValue(source.getValue());
+    }
+
+    public static void copy(PeriodType source, PeriodType target) {
+        if (target == null) {
+            target = new PeriodType();
+        }
+
+        if (source.getDurationMeasure() == null) {
+            target.setDurationMeasure(null);
+        } else {
+            if (target.getDurationMeasure() == null) {
+                target.setDurationMeasure(new QuantityType());
+            }
+            copy(source.getDurationMeasure(), target.getDurationMeasure());
+        }
+
+        target.setStartDateItem(null);
+        target.setStartDate(source.getStartDate());
+        target.setStartTimeItem(null);
+        target.setStartTime(source.getStartTime());
+        target.setEndDateItem(null);
+        target.setEndDate(source.getEndDate());
+        target.setEndTimeItem(null);
+        target.setEndTime(source.getEndTime());
+    }
+
+    public static void copy(QuantityType source, QuantityType target) {
+        if (target == null) {
+            target = new QuantityType();
+        }
+        target.setUnitCode(source.getUnitCode());
+        target.setUnitCodeListID(source.getUnitCodeListID());
+        target.setValue(source.getValue());
     }
 }
