bbceb1431391c2834a8528c96ec196c1cd1293f9
Suat Gonul <suat@srdc.com.tr>
2020-11-20 11:11:19 +0300

    Added metadata utility for managing entity metadata

---
+++ utility/src/main/java/eu/nimble/utility/persistence/resource/MetadataUtility.java
@@ -0,0 +1,61 @@
+package eu.nimble.utility.persistence.resource;
+
+import eu.nimble.service.model.ubl.commonaggregatecomponents.MetadataType;
+import eu.nimble.utility.exception.NimbleException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ * Utility providing methods on the generic {@link MetadataType} of managed entities
+ */
+public class MetadataUtility {
+    private static Logger logger = LoggerFactory.getLogger(MetadataUtility.class);
+
+
+    public static MetadataType createEntityMetadata(String userId, List<String> companyIds) throws NimbleException {
+        MetadataType metadata = new MetadataType();
+        metadata.setOwnerUser(userId);
+        metadata.setOwnerCompany(companyIds);
+
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTimeInMillis(System.currentTimeMillis());
+        try {
+            XMLGregorianCalendar currentDateTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            metadata.setCreationDate(currentDateTime);
+            metadata.setModificationDate(currentDateTime);
+        } catch (DatatypeConfigurationException e) {
+            String msg = "Could not create date for metadata";
+            logger.warn(msg, e);
+            throw new NimbleException(msg, e);
+        }
+        return metadata;
+    }
+
+    public static void updateEntityMetadata(MetadataType metadata) throws NimbleException {
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTimeInMillis(System.currentTimeMillis());
+        try {
+            XMLGregorianCalendar currentDateTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            metadata.setCreationDate(currentDateTime);
+            metadata.setModificationDate(currentDateTime);
+        } catch (DatatypeConfigurationException e) {
+            String msg = "Could not update date for metadata";
+            logger.warn(msg, e);
+            throw new NimbleException(msg, e);
+        }
+    }
+
+    public static boolean isOwnerCompany(String companyId, MetadataType metadata) {
+        if (metadata.getOwnerCompany() != null) {
+            return metadata.getOwnerCompany().contains(companyId);
+        } else {
+            return false;
+        }
+    }
+}
